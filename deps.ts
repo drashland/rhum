61ce5ed3a4013ac9a60bb78605d8b351074e9ccc
Edward Bebbington <edward.bebbington@intercity.technology>
2020-06-21 01:51:37 +0100

    [upgrade-deno] Updated deps

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { ServerRequest } from "https://deno.land/std@v0.57.0/http/server.ts";
+export { ServerRequest } from "https://deno.land/std@v0.58.0/http/server.ts";
 
-export { BufReader } from "https://deno.land/std@v0.57.0/io/bufio.ts";
+export { BufReader } from "https://deno.land/std@v0.58.0/io/bufio.ts";
 
-export * as asserts from "https://deno.land/std@v0.57.0/testing/asserts.ts";
+export * as asserts from "https://deno.land/std@v0.58.0/testing/asserts.ts";
