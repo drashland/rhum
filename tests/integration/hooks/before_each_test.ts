7f9f1f0bd0c9ba598a92ee1e76171c7b7783fa82
Edward Bebbington <edward.bebbington@intercity.technology>
2020-06-21 20:18:43 +0100

    [issue-#3-hooks] Added integration test for before each hooks

---
+++ tests/integration/hooks/before_each_test.ts
@@ -0,0 +1,43 @@
+/**
+ * Purpose of this test is to ensure a test using before_each succeeds
+ */
+
+import { Rhum } from "../../mod.ts";
+
+let suite_val = "Ed";
+let case_val = 22;
+
+Rhum.testPlan("app_test.ts", () => {
+  Rhum.beforeEach(() => {
+    suite_val = "Eric";
+  });
+  // Run the first test suite
+  Rhum.testSuite("run()", () => {
+    Rhum.beforeEach(() => {
+      case_val = 2;
+    });
+    Rhum.testCase("Returns true", () => {
+      Rhum.asserts.assertEquals(suite_val, "Eric");
+      suite_val = "Ed";
+      Rhum.asserts.assertEquals(case_val, 2);
+      case_val = 22
+    });
+    Rhum.testCase("Returns false", () => {
+      Rhum.asserts.assertEquals(case_val, 2)
+    })
+  });
+
+  // Run the second test suite
+  Rhum.testSuite("close()", () => {
+    Rhum.beforeEach(() => {
+      case_val = 0;
+    });
+    Rhum.testCase("Returns true", async () => {
+      Rhum.asserts.assertEquals(suite_val, "Eric");
+      suite_val = "Ed";
+      Rhum.asserts.assertEquals(case_val, 0);
+    });
+  });
+});
+
+Rhum.run();
