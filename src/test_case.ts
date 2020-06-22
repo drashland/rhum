5555752d2f34f5c4870ba0d6f1377c3ec54f880c
Eric Crooks <eric.crooks@gmail.com>
2020-06-22 07:18:01 -0400

    make changes based on code review

--- src/test_case.ts
+++ src/test_case.ts
@@ -1,12 +1,13 @@
 const encoder = new TextEncoder();
+import { ITestPlan, ITestCase } from "./interfaces.ts";
 
 /**
  * A class to help create uniform test case objects.
  */
 export class TestCase {
-  protected plan: any; // TODO(ebebbington) Use Plan interface
+  protected plan: ITestPlan;
 
-  constructor(plan: any) { // TODO(ebebbington) Use Plan interface
+  constructor(plan: ITestPlan) {
     this.plan = plan;
   }
 
@@ -16,7 +17,7 @@ export class TestCase {
     }
     Object.keys(this.plan.suites).forEach((suiteName) => {
       // Run cases
-      this.plan.suites[suiteName].cases.forEach(async (c: any) => {
+      this.plan!.suites[suiteName].cases!.forEach(async (c: ITestCase) => {
         // Run the case - required to run like this because the
         // hooks need to be ran inside the Deno.test call. Deno.test seems to queue
         // the tests, meaning all hooks are ran, and **then** the tests are ran
@@ -28,17 +29,17 @@ export class TestCase {
             this.plan.before_each_suite_hook();
           }
           if (this.plan.suites[suiteName].before_all_case_hook) {
-            this.plan.suites[suiteName].before_all_case_hook();
+            this.plan.suites[suiteName].before_all_case_hook!();
           }
           if (this.plan.suites[suiteName].before_each_case_hook) {
-            this.plan.suites[suiteName].before_each_case_hook();
+            this.plan.suites[suiteName].before_each_case_hook!();
           }
           await c.testFn();
           if (this.plan.suites[suiteName].after_each_case_hook) {
-            this.plan.suites[suiteName].after_each_case_hook();
+            this.plan.suites[suiteName].after_each_case_hook!();
           }
           if (this.plan.suites[suiteName].after_all_case_hook) {
-            this.plan.suites[suiteName].after_all_case_hook();
+            this.plan.suites[suiteName].after_all_case_hook!();
           }
           if (this.plan.after_each_suite_hook) {
             this.plan.after_each_suite_hook();
--- tests/unit/test_case_test.ts
+++ tests/unit/test_case_test.ts
@@ -1,19 +1,22 @@
 import { asserts } from "../../deps.ts";
 import { TestCase } from "../../src/test_case.ts";
+import { ITestPlan } from "../../src/interfaces.ts";
 
 Deno.test({
   name: "Unit | TestCase | run() | Runs the test without failing",
   async fn(): Promise<void> {
-    const plan = {
-      "suite_1": {
-        cases: [{
-          name: "case_1",
-          new_name: "123 case_1",
-          testFn: function () {
-            asserts.assertEquals(true, true);
-          },
-        }],
-      },
+    const plan: ITestPlan = {
+      suites: {
+        "suite_1": {
+          cases: [{
+            name: "case_1",
+            new_name: "123 case_1",
+            testFn: function () {
+              asserts.assertEquals(true, true);
+            },
+          }],
+        },
+      }
     };
     const testCase = new TestCase(plan);
     await testCase.run();
