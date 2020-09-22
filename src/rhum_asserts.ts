a7bf1ab3a16c3e50bf99a97ba49104cb07ced6fe
Edward Bebbington <edward.bebbington@intercity.technology>
2020-09-22 17:18:26 +0100

    [issue-#63-fix-asserts-intellisense] Add intellisense to method params

--- src/rhum_asserts.ts
+++ src/rhum_asserts.ts
@@ -1,5 +1,22 @@
 import { StdAsserts } from "../deps.ts";
 
-export const asserts = { ...StdAsserts };
+export const asserts = {
+  AssertionError: StdAsserts.AssertionError,
+  _format: StdAsserts._format,
+  assert: StdAsserts.assert,
+  assertArrayContains: StdAsserts.assertArrayContains,
+  assertEquals: StdAsserts.assertEquals,
+  assertNotEquals: StdAsserts.assertNotEquals,
+  assertNotMatch: StdAsserts.assertNotMatch,
+  assertNotStrictEquals: StdAsserts.assertNotStrictEquals,
+  assertStrictEquals: StdAsserts.assertStrictEquals,
+  assertStringContains: StdAsserts.assertStringContains,
+  assertThrows: StdAsserts.assertThrows,
+  assertThrowsAsync: StdAsserts.assertThrowsAsync,
+  equal: StdAsserts.equal,
+  fail: StdAsserts.fail,
+  unimplemented: StdAsserts.unimplemented,
+  unreachable: StdAsserts.unreachable
+};
 
-export type assertions = keyof typeof StdAsserts;
+//export type assertions = keyof typeof StdAsserts;
