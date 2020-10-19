b42253c978c7cfcdcb8e8f3a63a4d4cd416a26ba
Eric Crooks <eric.crooks@gmail.com>
2020-10-18 22:29:13 -0400

    tests:
    * getCurrentTestSuite
    * getCurrentTestSuiteNumTestCases
    * getTestPlan
    * setCurrentTestSuite
    * setCurrentTestSuiteNumTestCases

--- tests/unit/mod.ts
+++ tests/unit/mod.ts
@@ -122,4 +122,57 @@ Rhum.testPlan(() => {
       );
     });
   });
+
+  Rhum.testSuite("getCurrentTestSuite", () => {
+    Rhum.testCase("current_test_suite is set to ''", () => {
+      Rhum.setCurrentTestSuite("");
+      Rhum.asserts.assertEquals(Rhum.getCurrentTestSuite(), "");
+    });
+    Rhum.testCase("current_test_suite is set to 'some test suite'", () => {
+      Rhum.setCurrentTestSuite("some test suite");
+      Rhum.asserts.assertEquals(Rhum.getCurrentTestSuite(), "some test suite");
+    });
+  });
+
+  Rhum.testSuite("getCurrentTestSuiteNumTestCases", () => {
+    Rhum.testCase("current_test_suite_num_test_cases defaults to 0", () => {
+      Rhum.asserts.assertEquals(Rhum.getCurrentTestSuiteNumTestCases(), 0);
+    });
+    Rhum.testCase("current_test_suite_num_test_cases defaults to 0", () => {
+      Rhum.setCurrentTestSuiteNumTestCases(1);
+      Rhum.asserts.assertEquals(Rhum.getCurrentTestSuiteNumTestCases(), 1);
+    });
+  });
+
+  Rhum.testSuite("getTestPlan", () => {
+    Rhum.testCase("test_plan is set", () => {
+      Rhum.asserts.assertEquals(
+        Object.keys(Rhum.getTestPlan()),
+        [
+          "suites",
+          "before_all_suites_hook",
+          "before_each_suite_hook",
+          "after_each_suite_hook",
+          "after_all_suites_hook",
+        ],
+      );
+    });
+  });
+
+  Rhum.testSuite("setCurrentTestSuite", () => {
+    Rhum.testCase("current_test_suite is set to 'yo them shits stank'", () => {
+      Rhum.setCurrentTestSuite("yo them shits stank");
+      Rhum.asserts.assertEquals(
+        Rhum.getCurrentTestSuite(),
+        "yo them shits stank",
+      );
+    });
+  });
+
+  Rhum.testSuite("setCurrentTestSuiteNumTestCases", () => {
+    Rhum.testCase("current_test_suite_num_test_cases is set to 1337", () => {
+      Rhum.setCurrentTestSuiteNumTestCases(1337);
+      Rhum.asserts.assertEquals(Rhum.getCurrentTestSuiteNumTestCases(), 1337);
+    });
+  });
 });
