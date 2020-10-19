a51cd6aad6b369d7c08130034b76ed61d073d59d
Eric Crooks <eric.crooks@gmail.com>
2020-10-19 14:00:43 -0400

    add comments

--- tests/integration/output.ts
+++ tests/integration/output.ts
@@ -11,6 +11,22 @@ Rhum.testPlan(() => {
         stdout: "piped"
       });
       const stdout = new TextDecoder().decode(await p.output());
+
+      // When we assert the output below, we want to strip out anything that is
+      // filesystem related. For example, we want to strip the following out:
+      //
+      //     at Object.assertEquals (asserts.ts:196:9)
+      //     at Object.test_fn (file:///private/var/src/drashland/rhum/example_tests/test.ts:43:12)
+      //     at RhumRunner.runCase (file:///private/var/src/drashland/rhum/mod.ts:172:3)
+      //     at RhumRunner.runSuite (file:///private/var/src/drashland/rhum/mod.ts:248:8)
+      //     at async RhumRunner.runAllSuitesAndCases (file:///private/var/src/drashland/rhum/mod.ts:202:51)
+      //     at async RhumRunner.runTestPlan (file:///private/var/src/drashland/rhum/mod.ts:97:3)
+      //     at async RhumRunner.testPlan (file:///private/var/src/drashland/rhum/mod.ts:139:3)
+      //
+      // This information would be difficult to manage between a CI environment
+      // and a local environment. Therefore, we strip it out of the tests
+      // beacuse we don't really care about that stuff. We care about the
+      // output: PASS, FAIL, SKIP, and the Actual / Expected data.
       Rhum.asserts.assertEquals(
         stdout.replace(/.+at.*\)/g, ""),
         output
