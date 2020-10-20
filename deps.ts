37720eda85d93ef1b6f32d58c51b47104b13da3f
Eric Crooks <eric.crooks@gmail.com>
2020-10-20 12:03:08 -0400

    add new deps from cli service

--- deps.ts
+++ deps.ts
@@ -7,3 +7,9 @@ export { readLines } from "https://deno.land/std@0.74.0/io/mod.ts";
 export * as StdAsserts from "https://deno.land/std@0.74.0/testing/asserts.ts";
 
 export * as colors from "https://deno.land/std@0.74.0/fmt/colors.ts";
+
+export { logInfo, logError } from "https://raw.githubusercontent.com/drashland/services/master/cli/logger_service.ts";
+
+export { createHelpMenu } from "https://raw.githubusercontent.com/drashland/services/master/cli/help_menu_service.ts";
+
+export { commandRequiresArgs, commandIs } from "https://raw.githubusercontent.com/drashland/services/master/cli/command_service.ts";
