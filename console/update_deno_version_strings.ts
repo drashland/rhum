ed07c1da9f842e0314eabd836b4188c0cc6e8b87
Eric Crooks <eric.crooks@gmail.com>
2020-10-21 06:55:25 -0400

    add pre-release workflow; use PreReleaseCiService; use BumperCiService;

--- console/update_deno_version_strings.ts
+++ console/update_deno_version_strings.ts
@@ -1,79 +1,26 @@
-// Overview
-//
-// Updates deno version strings in files.
-// This scripts main purpose is to aid the `bumper`,
-// removing any extra manual work when we bump the deno version
+import { BumperCiService } from "https://raw.githubusercontent.com/drashland/services/master/ci/bumper_ci_service.ts";
 
-let fileContent = "";
-const decoder = new TextDecoder();
-const encoder = new TextEncoder();
+const b = new BumperCiService("rhum");
 
-const fetchResDeno = await fetch(
-  "https://cdn.deno.land/deno/meta/versions.json",
-);
-const denoVersions: {
-  latest: string;
-  versions: string[];
-} = await fetchResDeno.json(); // eg { latest: "v1.3.3", versions: ["v1.3.2", ...] }
-const latestDenoVersion = denoVersions.latest.replace("v", "");
-
-const fetchResRhum = await fetch(
-  "https://cdn.deno.land/rhum/meta/versions.json",
-);
-const rhumVersions: {
-  latest: string;
-  versions: string[];
-} = await fetchResRhum.json(); // eg { latest: "v1.3.3", versions: ["v1.3.2", ...] }
-const latestRhumVersion = denoVersions.latest;
-
-// Master workflow
-fileContent = decoder.decode(
-  await Deno.readFile("./.github/workflows/master.yml"),
-);
-fileContent = fileContent.replace(
-  /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
-  `deno: ["${latestDenoVersion}"]`,
-);
-await Deno.writeFile(
-  "./.github/workflows/master.yml",
-  encoder.encode(fileContent),
-);
-
-// Bumper workflow
-fileContent = decoder.decode(
-  await Deno.readFile("./.github/workflows/bumper.yml"),
-);
-fileContent = fileContent.replace(
-  /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
-  `deno: ["${latestDenoVersion}"]`,
-);
-await Deno.writeFile(
-  "./.github/workflows/bumper.yml",
-  encoder.encode(fileContent),
-);
-
-// mod.ts
-fileContent = decoder.decode(
-  await Deno.readFile("./mod.ts"),
-);
-fileContent = fileContent.replace(
-  /version = ".+"/,
-  `version = "${latestRhumVersion}"`,
-);
-await Deno.writeFile(
-  "./mod.ts",
-  encoder.encode(fileContent),
-);
-
-// src/cli/commands/make.ts
-fileContent = decoder.decode(
-  await Deno.readFile("./src/cli/commands/make.ts"),
-);
-fileContent = fileContent.replace(
-  `import { Rhum } from "https://deno.land/x/rhum@v1.1.4/mod.ts`,
-  `import { Rhum } from "https://deno.land/x/rhum@${latestRhumVersion}/mod.ts"`,
-);
-await Deno.writeFile(
-  "./src/cli/commands/make.ts",
-  encoder.encode(fileContent),
-);
+b.bump([
+  {
+    filename: "./.github/workflows/master.yml",
+    replaceTheRegex: /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
+  },
+  {
+    filename: "./.github/workflows/bumper.yml",
+    replaceTheRegex: /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
+  },
+  {
+    filename: "./mod.ts",
+    replaceTheRegex: /version = ".+"/,
+    replaceWith: `version = "{{ thisModulesLatestVersion }}"`,
+  },
+  {
+    filename: "./src/cli/commands/make.ts",
+    replaceTheRegex: `import { Rhum } from "https://deno.land/x/rhum@v1.1.4/mod.ts`,
+    replaceWith: `import { Rhum } from "https://deno.land/x/rhum@{{ thisModulesLatestVersion }}/mod.ts"`,
+  },
+]);
