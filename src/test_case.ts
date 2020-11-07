47c1e4c2f08674e18188642edb31cb8dbfeb9eda
Eric Crooks <eric.crooks@gmail.com>
2020-11-06 22:02:43 -0500

    add flags to check if execution of hooks

--- src/test_case.ts
+++ src/test_case.ts
@@ -25,37 +25,58 @@ export class TestCase {
     if (this.plan.hasOwnProperty("suites") === false) {
       return;
     }
+
+    // Track the execution of hooks
+    let executedBeforeAllSuiteHook = false;
+    let executedAfterAllSuiteHook = false;
+
     Object.keys(this.plan.suites).forEach((suiteName) => {
+
+      // Track the execution of hooks
+      let executedBeforeEachSuiteHook = false;
+      let executedAfterEachSuiteHook = false;
+      let executedBeforeAllCaseHook = false;
+      let executedAfterAllCaseHook = false;
+
       // Run cases
       this.plan!.suites[suiteName].cases!.forEach(async (c: ITestCase) => {
         // Run the case - required to run like this because the
         // hooks need to be ran inside the Deno.test call. Deno.test seems to queue
         // the tests, meaning all hooks are ran, and **then** the tests are ran
         const hookAttachedTestFn = async () => {
-          if (this.plan.before_all_suite_hook) {
+          if (this.plan.before_all_suite_hook && !executedBeforeAllSuiteHook) {
             await this.plan.before_all_suite_hook();
+            executedBeforeAllSuiteHook = true;
           }
-          if (this.plan.before_each_suite_hook) {
+          if (this.plan.before_each_suite_hook && !executedBeforeEachSuiteHook) {
             await this.plan.before_each_suite_hook();
+            executedBeforeEachSuiteHook = true;
           }
-          if (this.plan.suites[suiteName].before_all_case_hook) {
+          if (this.plan.suites[suiteName].before_all_case_hook && !executedBeforeAllCaseHook) {
             await this.plan.suites[suiteName].before_all_case_hook!();
+            executedBeforeAllCaseHook = true;
           }
           if (this.plan.suites[suiteName].before_each_case_hook) {
             await this.plan.suites[suiteName].before_each_case_hook!();
           }
+
           await c.testFn();
+
           if (this.plan.suites[suiteName].after_each_case_hook) {
             await this.plan.suites[suiteName].after_each_case_hook!();
           }
-          if (this.plan.suites[suiteName].after_all_case_hook) {
+          if (this.plan.suites[suiteName].after_all_case_hook &&
+              !executedAfterAllCaseHook) {
             await this.plan.suites[suiteName].after_all_case_hook!();
+            executedAfterAllCaseHook = true;
           }
-          if (this.plan.after_each_suite_hook) {
+          if (this.plan.after_each_suite_hook && !executedAfterEachSuiteHook) {
             await this.plan.after_each_suite_hook();
+            executedAfterEachSuiteHook = true;
           }
-          if (this.plan.after_all_suite_hook) {
+          if (this.plan.after_all_suite_hook && !executedAfterAllSuiteHook) {
             await this.plan.after_all_suite_hook();
+            executedAfterAllSuiteHook = true;
           }
         };
         // (ebebbington) To stop the output of test running being horrible
