1d242de254b8ddc421e0c50c2ca54041e17413c1
Luca Casonato <lucacasonato@yahoo.com>
2020-12-31 13:45:49 +0000

    add test

--- tests/integration/hooks/after_all_test.ts
+++ tests/integration/hooks/after_all_test.ts
@@ -4,58 +4,66 @@
 
 import { Rhum } from "../../../mod.ts";
 
-let suite_val = "Ed";
-let case_val = 22;
-let async_case_val = 5;
+let suite_val = 0;
+let case_val = 0;
+let async_case_val = 0;
 Rhum.testPlan("after_all_test.ts", () => {
   Rhum.afterAll(() => {
-    suite_val = "Eric";
+    Rhum.asserts.assertEquals(suite_val, 3);
   });
   // Run the first test suite
   Rhum.testSuite("test suite 1", () => {
     Rhum.afterAll(() => {
-      case_val = 2;
+      Rhum.asserts.assertEquals(case_val, 2);
+      case_val = 3;
     });
     Rhum.testCase("hooks properly set suite_val and case_val", () => {
-      // Asserting that the suite val should not have been changed by the hook (yet)
-      Rhum.asserts.assertEquals(suite_val, "Ed");
-      // Revert it back for the next test that uses it
-      suite_val = "Ed";
-      // Assert the value is kept the same (hook hasn't ran yet)
-      Rhum.asserts.assertEquals(case_val, 22);
+      // Asserting that the suite val should not have been changed by the hook
+      Rhum.asserts.assertEquals(suite_val, 0);
+      suite_val = 1;
+      // Asserting that the case val should not have been changed by the hook
+      Rhum.asserts.assertEquals(case_val, 0);
+      case_val = 1;
     });
     Rhum.testCase("hooks properly set case_val", () => {
-      // Assert the value has changes as the hook should have ran
-      Rhum.asserts.assertEquals(case_val, 2);
+      // Asserting that the suite val should not have been changed by the hook
+      Rhum.asserts.assertEquals(suite_val, 1);
+      suite_val = 2;
+      // Asserting that the case val should not have been changed by the hook
+      Rhum.asserts.assertEquals(case_val, 1);
+      case_val = 2;
     });
   });
 
   // Run the second test suite
   Rhum.testSuite("test suite 2", () => {
     Rhum.afterAll(() => {
-      case_val = 0;
+      Rhum.asserts.assertEquals(case_val, 4);
     });
     Rhum.testCase("hooks properly set suite_val and case_val", async () => {
-      // Asserting the hook should have replaced the name
-      Rhum.asserts.assertEquals(suite_val, "Eric");
-      suite_val = "Ed";
-      // Should be kept the same as the after each hook should have updated the value
-      Rhum.asserts.assertEquals(case_val, 2);
+      // Asserting that the suite val should not have been changed by the hook
+      Rhum.asserts.assertEquals(suite_val, 2);
+      suite_val = 3;
+      // Asserting that the case val has been changed by the hook
+      Rhum.asserts.assertEquals(case_val, 3);
+      case_val = 4;
     });
   });
   Rhum.testSuite("test suite 3", () => {
     Rhum.afterAll(async () => {
+      Rhum.asserts.assertEquals(async_case_val, 1);
       await new Promise((resolve) => {
-        setTimeout(() => resolve((async_case_val = 15)), 1000);
+        setTimeout(() => resolve((async_case_val = 2)), 1000);
       });
     });
     Rhum.testCase("Async afterAll hook has no effect before case", () => {
-      Rhum.asserts.assertEquals(async_case_val, 5);
+      Rhum.asserts.assertEquals(async_case_val, 0);
+      async_case_val = 1;
     });
   });
   Rhum.testSuite("test suite 4", () => {
     Rhum.testCase("Async afterAll hook has effect after case", () => {
-      Rhum.asserts.assertEquals(async_case_val, 15);
+      Rhum.asserts.assertEquals(async_case_val, 2);
     });
   });
 });
