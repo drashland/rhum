3115408b4f5c9658897730c68a97af3050aef4f2
Luca Casonato <lucacasonato@yahoo.com>
2020-12-31 13:47:03 +0000

    fmt

--- src/test_case.ts
+++ src/test_case.ts
@@ -38,71 +38,82 @@ export class TestCase {
       let executedAfterAllCaseHook = false;
 
       // Run cases
-      this.plan!.suites[suiteName].cases!.forEach(async (c: ITestCase, caseIndex) => {
-        // Run the case - required to run like this because the
-        // hooks need to be ran inside the Deno.test call. Deno.test seems to queue
-        // the tests, meaning all hooks are ran, and **then** the tests are ran
-        const hookAttachedTestFn = async () => {
-          if (this.plan.before_all_suite_hook && !executedBeforeAllSuiteHook) {
-            await this.plan.before_all_suite_hook();
-            executedBeforeAllSuiteHook = true;
-          }
-          if (
-            this.plan.before_each_suite_hook && !executedBeforeEachSuiteHook
-          ) {
-            await this.plan.before_each_suite_hook();
-            executedBeforeEachSuiteHook = true;
-          }
-          if (
-            this.plan.suites[suiteName].before_all_case_hook &&
-            !executedBeforeAllCaseHook
-          ) {
-            await this.plan.suites[suiteName].before_all_case_hook!();
-            executedBeforeAllCaseHook = true;
-          }
-          if (this.plan.suites[suiteName].before_each_case_hook) {
-            await this.plan.suites[suiteName].before_each_case_hook!();
-          }
+      this.plan!.suites[suiteName].cases!.forEach(
+        async (c: ITestCase, caseIndex) => {
+          // Run the case - required to run like this because the
+          // hooks need to be ran inside the Deno.test call. Deno.test seems to queue
+          // the tests, meaning all hooks are ran, and **then** the tests are ran
+          const hookAttachedTestFn = async () => {
+            if (
+              this.plan.before_all_suite_hook && !executedBeforeAllSuiteHook
+            ) {
+              await this.plan.before_all_suite_hook();
+              executedBeforeAllSuiteHook = true;
+            }
+            if (
+              this.plan.before_each_suite_hook && !executedBeforeEachSuiteHook
+            ) {
+              await this.plan.before_each_suite_hook();
+              executedBeforeEachSuiteHook = true;
+            }
+            if (
+              this.plan.suites[suiteName].before_all_case_hook &&
+              !executedBeforeAllCaseHook
+            ) {
+              await this.plan.suites[suiteName].before_all_case_hook!();
+              executedBeforeAllCaseHook = true;
+            }
+            if (this.plan.suites[suiteName].before_each_case_hook) {
+              await this.plan.suites[suiteName].before_each_case_hook!();
+            }
 
-          await c.testFn();
+            await c.testFn();
 
-          if (this.plan.suites[suiteName].after_each_case_hook) {
-            await this.plan.suites[suiteName].after_each_case_hook!();
+            if (this.plan.suites[suiteName].after_each_case_hook) {
+              await this.plan.suites[suiteName].after_each_case_hook!();
+            }
+            const isLastCase =
+              (this.plan!.suites[suiteName].cases!.length - 1) == caseIndex;
+            if (
+              this.plan.suites[suiteName].after_all_case_hook &&
+              !executedAfterAllCaseHook && isLastCase
+            ) {
+              await this.plan.suites[suiteName].after_all_case_hook!();
+              executedAfterAllCaseHook = true;
+            }
+            if (
+              this.plan.after_each_suite_hook && !executedAfterEachSuiteHook
+            ) {
+              await this.plan.after_each_suite_hook();
+              executedAfterEachSuiteHook = true;
+            }
+            const isLastSuite =
+              (Object.keys(this.plan!.suites).length - 1) == suiteIndex;
+            if (
+              this.plan.after_all_suite_hook && !executedAfterAllSuiteHook &&
+              isLastSuite
+            ) {
+              await this.plan.after_all_suite_hook();
+              executedAfterAllSuiteHook = true;
+            }
+          };
+          // (ebebbington) To stop the output of test running being horrible
+          // in the CI, we will only display the new name which should be
+          // "plan | suite " case", as opposed to the "super saiyan"
+          // version. This name is generated differently inside `formatTestCaseName`
+          // based on if the tests are being ran inside a CI job
+          if (Deno.env.get("CI") === "true") {
+            await Deno.test(c.new_name, async () => {
+              await hookAttachedTestFn();
+            });
+          } else {
+            await Deno.test(c.name, async () => {
+              Deno.stdout.writeSync(encoder.encode(c.new_name));
+              await hookAttachedTestFn();
+            });
           }
-          const isLastCase = (this.plan!.suites[suiteName].cases!.length - 1) == caseIndex;
-          if (
-            this.plan.suites[suiteName].after_all_case_hook &&
-            !executedAfterAllCaseHook && isLastCase
-          ) {
-            await this.plan.suites[suiteName].after_all_case_hook!();
-            executedAfterAllCaseHook = true;
-          }
-          if (this.plan.after_each_suite_hook && !executedAfterEachSuiteHook) {
-            await this.plan.after_each_suite_hook();
-            executedAfterEachSuiteHook = true;
-          }
-          const isLastSuite = (Object.keys(this.plan!.suites).length - 1) == suiteIndex;
-          if (this.plan.after_all_suite_hook && !executedAfterAllSuiteHook && isLastSuite) {
-            await this.plan.after_all_suite_hook();
-            executedAfterAllSuiteHook = true;
-          }
-        };
-        // (ebebbington) To stop the output of test running being horrible
-        // in the CI, we will only display the new name which should be
-        // "plan | suite " case", as opposed to the "super saiyan"
-        // version. This name is generated differently inside `formatTestCaseName`
-        // based on if the tests are being ran inside a CI job
-        if (Deno.env.get("CI") === "true") {
-          await Deno.test(c.new_name, async () => {
-            await hookAttachedTestFn();
-          });
-        } else {
-          await Deno.test(c.name, async () => {
-            Deno.stdout.writeSync(encoder.encode(c.new_name));
-            await hookAttachedTestFn();
-          });
-        }
-      });
+        },
+      );
     });
   }
 }
