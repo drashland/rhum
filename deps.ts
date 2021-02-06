7d7f73371606ce7d7a5e00f4fbaf901df5942fd2
crookse <crookse@users.noreply.github.com>
2021-02-06 00:31:52 +0000

    Update dependencies and bump version numbers across repo

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { ServerRequest } from "https://deno.land/std@0.84.0/http/server.ts";
+export { ServerRequest } from "https://deno.land/std@0.86.0/http/server.ts";
 
-export { BufReader } from "https://deno.land/std@0.84.0/io/bufio.ts";
+export { BufReader } from "https://deno.land/std@0.86.0/io/bufio.ts";
 
-export * as StdAsserts from "https://deno.land/std@0.84.0/testing/asserts.ts";
+export * as StdAsserts from "https://deno.land/std@0.86.0/testing/asserts.ts";
 
-export * as colors from "https://deno.land/std@0.84.0/fmt/colors.ts";
+export * as colors from "https://deno.land/std@0.86.0/fmt/colors.ts";
