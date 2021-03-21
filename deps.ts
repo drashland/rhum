c592da1cf77b9447d1e29e83b9eb4d77dae9aad0
Eric Crooks <eric.crooks@gmail.com>
2021-03-21 11:03:57 -0400

    update deps

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
-export { ServerRequest } from "https://deno.land/std@0.87.0/http/server.ts";
+export { ServerRequest } from "https://deno.land/std@0.89.0/http/server.ts";
 
-export { BufReader } from "https://deno.land/std@0.87.0/io/bufio.ts";
+export { BufReader } from "https://deno.land/std@0.89.0/io/bufio.ts";
 
-export { readLines } from "https://deno.land/std@0.87.0/io/mod.ts";
+export { readLines } from "https://deno.land/std@0.89.0/io/mod.ts";
 
-export * as StdAsserts from "https://deno.land/std@0.87.0/testing/asserts.ts";
+export * as StdAsserts from "https://deno.land/std@0.89.0/testing/asserts.ts";
 
-export * as colors from "https://deno.land/std@0.87.0/fmt/colors.ts";
+export * as colors from "https://deno.land/std@0.89.0/fmt/colors.ts";
 
-export { walkSync } from "https://deno.land/std@0.87.0/fs/walk.ts";
+export { walkSync } from "https://deno.land/std@0.89.0/fs/walk.ts";
 
 export {
   Line,
