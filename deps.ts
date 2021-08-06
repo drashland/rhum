870043b188928f2313969084fe7f423683210477
Eric Crooks <eric.crooks@gmail.com>
2021-08-05 23:21:02 -0400

    chore: update deps; merge master

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { ServerRequest } from "https://deno.land/std@0.97.0/http/server.ts";
+export { ServerRequest } from "https://deno.land/std@0.103.0/http/server.ts";
 
-export { BufReader } from "https://deno.land/std@0.97.0/io/bufio.ts";
+export { BufReader } from "https://deno.land/std@0.103.0/io/bufio.ts";
 
-export * as StdAsserts from "https://deno.land/std@0.97.0/testing/asserts.ts";
+export * as StdAsserts from "https://deno.land/std@0.103.0/testing/asserts.ts";
 
-export * as colors from "https://deno.land/std@0.97.0/fmt/colors.ts";
+export * as colors from "https://deno.land/std@0.103.0/fmt/colors.ts";
