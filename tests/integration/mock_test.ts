9f69279383d6934b0bc8abbfab47e2f7878c0f46
Eric Crooks <eric.crooks@gmail.com>
2021-11-30 19:31:09 -0500

    test: add integration test for native request object

--- tests/integration/mock_test.ts
+++ tests/integration/mock_test.ts
@@ -23,6 +23,23 @@ class TestObject {
   }
 }
 
+class TestRequestHandler {
+  async handle(request: Request): Promise<string | Error> {
+    const method = request.method.toLowerCase();
+    const contentType = request.headers.get("Content-Type");
+
+    if (method !== "post") {
+      return "Method is not post";
+    }
+
+    if (contentType !== "application/json") {
+      return "Content-Type is incorrect";
+    }
+
+    return "posted";
+  }
+}
+
 Rhum.testPlan("mock_test.ts", () => {
   Rhum.testSuite("mock()", () => {
     Rhum.testCase("can mock an object", () => {
@@ -74,6 +91,34 @@ Rhum.testPlan("mock_test.ts", () => {
       Rhum.asserts.assertEquals(mockMathService.calls.add, 0);
       mockTestObject.sum(1, 1);
       Rhum.asserts.assertEquals(mockMathService.calls.add, 1);
+    });
+
+    Rhum.testCase("Native Request mock", async () => {
+      const router = Rhum.mock(TestRequestHandler).create();
+
+      const reqPost = new Request("https://google.com", {
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+      Rhum.asserts.assertEquals(router.calls.handle, 0);
+      Rhum.asserts.assertEquals(await router.handle(reqPost), "posted");
+      Rhum.asserts.assertEquals(router.calls.handle, 1);
+
+      const reqPostNotJson = new Request("https://google.com", {
+        method: "post",
+      });
+      Rhum.asserts.assertEquals(router.calls.handle, 1);
+      Rhum.asserts.assertEquals(await router.handle(reqPostNotJson), "Content-Type is incorrect");
+      Rhum.asserts.assertEquals(router.calls.handle, 2);
+
+      const reqGet = new Request("https://google.com", {
+        method: "get",
+      });
+      Rhum.asserts.assertEquals(router.calls.handle, 2);
+      Rhum.asserts.assertEquals(await router.handle(reqGet), "Method is not post");
+      Rhum.asserts.assertEquals(router.calls.handle, 3);
     });
   });
 });
