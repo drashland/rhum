dcce1305600ec896d8f2c8a6c9098f6a1cd9ee01
drashbot <59404681+drashbot@users.noreply.github.com>
2022-03-31 14:57:17 -0400

    Update dependencies and bump version numbers across repo (#142)
    Co-authored-by: crookse <crookse@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { BufReader } from "https://deno.land/std@0.131.0/io/bufio.ts";
+export { BufReader } from "https://deno.land/std@0.132.0/io/bufio.ts";
 
-export * as StdAsserts from "https://deno.land/std@0.131.0/testing/asserts.ts";
+export * as StdAsserts from "https://deno.land/std@0.132.0/testing/asserts.ts";
 
-export * as colors from "https://deno.land/std@0.131.0/fmt/colors.ts";
+export * as colors from "https://deno.land/std@0.132.0/fmt/colors.ts";
