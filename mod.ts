15dd9cd7747bfbe5089b0ef01c62fcaaf5b02b4a
Eric Crooks <eric.crooks@gmail.com>
2022-04-11 23:20:52 -0400

    chore: state Gerard Meszaros in doc blocks

--- mod.ts
+++ mod.ts
@@ -6,8 +6,9 @@ export * as Interfaces from "./src/interfaces.ts";
 /**
  * Create a dummy.
  *
- * Per Martin Fowler, "Dummy objects are passed around but never actually used.
- * Usually they are just used to fill parameter lists."
+ * Per Martin Fowler (based on Gerard Meszaros), "Dummy objects are passed
+ * around but never actually used.  Usually they are just used to fill parameter
+ * lists."
  *
  * @param constructorFn - The constructor function to use to become the
  * prototype of the dummy. Dummy objects should be the same instance as what
@@ -25,9 +26,9 @@ export const Dummy = <T>(constructorFn?: Constructor<T>): T => {
 /**
  * Get the builder to create fake objects.
  *
- * Per Martin Fowler, "Fake objects actually have working implementations, but
- * usually take some shortcut which makes them not suitable for production (an
- * InMemoryTestDatabase is a good example)."
+ * Per Martin Fowler (based on Gerard Meszaros), "Fake objects actually have
+ * working implementations, but usually take some shortcut which makes them not
+ * suitable for production (an InMemoryTestDatabase is a good example)."
  *
  * @param constructorFn - The constructor function of the object to fake.
  *
@@ -40,10 +41,11 @@ export const Fake = <T>(constructorFn: Constructor<T>): FakeBuilder<T> => {
 /**
  * Get the builder to create mocked objects.
  *
- * Per Martin Fowler, "Mocks are pre-programmed with expectations which form a
- * specification of the calls they are expected to receive. They can throw an
- * exception if they receive a call they don't expect and are checked during
- * verification to ensure they got all the calls they were expecting."
+ * Per Martin Fowler (based on Gerard Meszaros), "Mocks are pre-programmed with
+ * expectations which form a specification of the calls they are expected to
+ * receive. They can throw an exception if they receive a call they don't expect
+ * and are checked during verification to ensure they got all the calls they
+ * were expecting."
  *
  * @param constructorFn - The constructor function of the object to mock.
  *
@@ -56,9 +58,9 @@ export const Mock = <T>(constructorFn: Constructor<T>): MockBuilder<T> => {
 /**
  * Create a stub.
  *
- * Per Martin Fowler, "Stubs provide canned answers to calls made during the
- * test, usually not responding at all to anything outside what's programmed in
- * for the test."
+ * Per Martin Fowler (based on Gerard Meszaros), "Stubs provide canned answers
+ * to calls made during the test, usually not responding at all to anything
+ * outside what's programmed in for the test."
  *
  * @param obj -The object containing the member to stub.
  */
