6299ba2a281391f63ffbc78b0e71b4394c95d463
Eric Crooks <eric.crooks@gmail.com>
2022-04-11 21:20:57 -0400

    feat: add dummy logic

--- mod.ts
+++ mod.ts
@@ -3,6 +3,22 @@ import { MockBuilder } from "./src/mock_builder.ts";
 
 export type { Constructor, Stubbed } from "./src/types.ts";
 export { MockBuilder } from "./src/mock_builder.ts";
+/**
+ * Create an object that can be passed around, but never actually used. A dummy
+ * is usually just used to fill a parameter.
+ *
+ * @param constructorFn - The constructor function to use to become the
+ * prototype of the dummy. Dummy objects should be the same instance as what
+ * they are standing in for. For example, if a `SomeClass` parameter needs to be
+ * filled with a dummy because it is out of scope for a test, then the dummy
+ * should be an instance of `SomeClass`.
+ * @returns A dummy object being an instance of the given constructor function.
+ */
+export const Dummy = <T>(constructorFn?: Constructor<T>): T => {
+  const dummy = Object.create({});
+  Object.setPrototypeOf(dummy, constructorFn ?? Object);
+  return dummy;
+}
 
 /**
  * Get the builder to help you create mocked objects.
