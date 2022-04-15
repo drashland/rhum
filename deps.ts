f3302ad22c1959491deac9242eeefb326f649634
drashbot <59404681+drashbot@users.noreply.github.com>
2022-04-15 00:32:28 +0000

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { BufReader } from "https://deno.land/std@0.134.0/io/bufio.ts";
+export { BufReader } from "https://deno.land/std@0.135.0/io/bufio.ts";
 
-export * as StdAsserts from "https://deno.land/std@0.134.0/testing/asserts.ts";
+export * as StdAsserts from "https://deno.land/std@0.135.0/testing/asserts.ts";
 
-export * as colors from "https://deno.land/std@0.134.0/fmt/colors.ts";
+export * as colors from "https://deno.land/std@0.135.0/fmt/colors.ts";
